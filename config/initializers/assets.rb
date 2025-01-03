806562a7c2ae6849feb7ad9c589ca358260ee139
John Adams <jna@retina.net>
2025-01-03 10:50:37 -0800

    remove all of the scss generation and fall back to minimized css and js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,9 @@ Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
+
+# forcing some things here for our templates
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "stylesheets")
+
 Rails.application.config.assets.precompile << "bootstrap.min.js"
